830482e9281255186261bf26eaf8f8175df95204
Opilane TTHK <opilane@tthk.ee>
2020-12-09 15:44:01 +0200

    first commit

---
+++ MilanaCat/Cat.cs
@@ -0,0 +1,16 @@
+﻿namespace MilanaCat
+{
+    internal class Cat
+    {
+        private string v1;
+        private string v2;
+
+        public Cat(string v1, string v2)
+        {
+            this.v1 = v1;
+            this.v2 = v2;
+        }
+
+        public int Hungry { get; internal set; }
+    }
+}
---
+++ MilanaCat/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace MilanaCat
+{
+    class Program
+    {
+
+
+        class Cat
+        {
+            string name;
+            string color;
+            double hungry;
+
+            public Cat(string _name, string _color)
+            {
+                name = _name;
+                color = _color;
+                hungry = 0;
+                Console.WriteLine($"The cat {name} has been created.");
+            }
+            public double Hungry
+            {
+                get { return hungry; }
+            }
+
+
+
+            public void Sleep()
+            {
+                Console.WriteLine("Zzzzzz");
+                hungry += 0.2;
+            }
+            public void SayMeow()
+            {
+                Console.WriteLine("Meow-Meow");
+            }
+            public void Rename(string newName)
+            {
+                name = newName;
+            }
+
+            public void ShowCatData()
+            {
+                Console.WriteLine($"Name: {name}; Color: {color}; Hungry: {hungry};");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Cat newCat = new Cat("Missi", "Yellow");
+
+            Console.WriteLine($"The level of hungry: {newCat.Hungry}");
+
+            while (newCat.Hungry != 1)
+            {
+               newCat.Sleep(); 
+            }
+
+            newCat.SayMeow();
+
+            Console.WriteLine("Name the cat: ");
+            string newCatName = Console.ReadLine();
+            newCat.Rename(newCatName);
+            newCat.ShowCatData();
+
+        }
+    }
+}
+
+    
+
